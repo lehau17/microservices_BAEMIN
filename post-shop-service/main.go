a1e2724e6a3e7232b59d10a58cab7ee146282544
lehau17 <121428266+lehau17@users.noreply.github.com>
2024-12-20 10:17:10 +0700

    Set up config

--- post-shop-service/main.go
+++ post-shop-service/main.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+	"post-shop-service/config"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
-	"github.com/joho/godotenv"
 )
 
 var db *sqlx.DB
 
 func main() {
-    // Load .env file
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatalf("Error loading .env file: %v", err)
-    }
-
-    // Database connection
-    dbUser := os.Getenv("DB_USER")
-    dbPassword := os.Getenv("DB_PASSWORD")
-    dbHost := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
-    dbName := os.Getenv("DB_NAME")
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
-
-    db, err = sqlx.Connect("mysql", dsn)
-    if err != nil {
-        log.Fatalf("Failed to connect to database: %v", err)
-    }
-    log.Println("Connected to database!")
-
-    // Initialize Gin
+	config.NewLoadConfigENV()
+	config.NewSqlInstance()
+	gin.SetMode(gin.DebugMode)
     r := gin.Default()
+
+	
 
     // Routes
     r.GET("/users", getUsers)
