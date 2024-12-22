9e1ae15f12a719e237d3fbb1ecd10ab6add057ba
lehau17 <121428266+lehau17@users.noreply.github.com>
2024-12-22 06:26:39 -0300

    Add storage post-shop-service method inc and des one like

---
+++ post-shop-service/module/post/storage/descrease_one_like.go
@@ -0,0 +1,21 @@
+package poststorage
+
+import (
+	"context"
+	"post-shop-service/common"
+	httpstatus "post-shop-service/common/http_status"
+)
+
+func (s *sqlStore) DescreaseLike(ctx context.Context, postID int) error {
+	query := `UPDATE post
+		SET total_like = total_like - 1
+		WHERE id = ? and status = 'published'`
+	if result, err := s.db.ExecContext(ctx, query, postID); err != nil {
+		rowAffect, _ := result.RowsAffected()
+		if rowAffect == 0 {
+			return common.NewErrorRpcResponse(httpstatus.StatusBadRequest, err)
+		}
+		return common.NewErrorRpcResponse(httpstatus.StatusInternalServerError, err)
+	}
+	return nil
+}
