0eefd9643fa4f5c7e684e8e4fd0c9075ccd6befc
lehau17 <121428266+lehau17@users.noreply.github.com>
2024-12-23 09:09:07 +0700

    Fixing descrease one like

--- post-shop-service/module/post/storage/descrease_one_like.go
+++ post-shop-service/module/post/storage/descrease_one_like.go
@@ -10,15 +10,14 @@ import (
 func (s *sqlStore) DescreaseLike(ctx context.Context, postID int) error {
 	query := `UPDATE post
 		SET total_like = total_like - 1
-		WHERE id = ? and status = 'published'`
+		WHERE id = ? and status = 'published' and total_like > 0`
 	result, err := s.db.ExecContext(ctx, query, postID)
 	if err != nil {
-
 		return common.NewErrorRpcResponse(httpstatus.StatusInternalServerError, err)
 	}
 	rowAffect, _ := result.RowsAffected()
 	if rowAffect == 0 {
-		return common.NewErrorRpcResponse(httpstatus.StatusBadRequest, errors.New("Post not found"))
+		return common.NewErrorRpcResponse(httpstatus.StatusBadRequest, errors.New("post not found"))
 	}
 	return nil
 }
