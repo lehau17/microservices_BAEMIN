d3124214b18472bc433284587a0f2be71ba7289e
lehau17 <121428266+lehau17@users.noreply.github.com>
2024-12-23 14:47:08 +0700

    Add handler find all by post

--- comment-post-shop-service/component/handler_message_broken/handler.go
+++ comment-post-shop-service/component/handler_message_broken/handler.go
@@ -2,11 +2,13 @@ package handlermessagebroken
 
 import (
 	"comment-post-shop-service/common"
+	"comment-post-shop-service/common/paging"
 	appcontext "comment-post-shop-service/component/app_context"
 	commentmodel "comment-post-shop-service/module/comment/model"
 	messagebrokenrabbitmq "comment-post-shop-service/module/comment/tranport/message_broken_rabbitMQ"
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/streadway/amqp"
 )
@@ -58,68 +60,44 @@ func HandlerMessageBroken(payloadQueue *common.PayloadQueue, d *amqp.Delivery, a
 			}
 			messagebrokenrabbitmq.UpdateComment(appCtx, &comment, d)
 		}
-		// if pagingData, ok := payloadQueue.Data.(map[string]interface{}); ok {
 
-		// 	var p paging.Paging
-		// 	if page, ok := pagingData["page"].(string); ok {
-		// 		page, _ := strconv.Atoi(page)
-		// 		p.Page = page
-		// 	}
-		// 	if limit, ok := pagingData["limit"].(string); ok {
-		// 		limit, _ := strconv.Atoi(limit)
-		// 		p.Limit = limit
-		// 	}
-
-		// 	if cursor, ok := pagingData["cursor"].(string); ok {
-		// 		cursor, err := strconv.Atoi(cursor)
-		// 		if err != nil {
-		// 			log.Printf("Failed to convert cursor to int: %v", err)
-		// 		} else {
-		// 			p.Cursor = &cursor
-		// 		}
-		// 	}
-		// 	p.Validate()
-		// 	posttranport.FindAllPost(appCtx, &p, d)
-		// } else {
-		// 	log.Printf("Data in payloadQueue is not of type map[string]interface{}")
-		// }
 	case "delete_post":
 		// if id, ok := payloadQueue.Data.(float64); ok {
 		// 	posttranport.DeletePost(appCtx, int64(id), d)
 		// } else {
 		// 	log.Printf("Data in payloadQueue is not of type int64")
 		// }
-	case "find_all_by_shop":
-		// if pagingData, ok := payloadQueue.Data.(map[string]interface{}); ok {
-		// 	fmt.Printf("pagingData: %v\n %t", pagingData, pagingData["shop_id"])
-		// 	var p paging.Paging
-		// 	var shop_id int
-		// 	if page, ok := pagingData["page"].(string); ok {
-		// 		page, _ := strconv.Atoi(page)
-		// 		p.Page = page
-		// 	}
-		// 	if shop_id_payload, ok := pagingData["shop_id"].(float64); ok {
-		// 		shop_id_int := int(shop_id_payload)
-		// 		shop_id = (shop_id_int)
-		// 	}
-		// 	if limit, ok := pagingData["limit"].(string); ok {
-		// 		limit, _ := strconv.Atoi(limit)
-		// 		p.Limit = limit
-		// 	}
+	case "find_all_by_post":
+		if pagingData, ok := payloadQueue.Data.(map[string]interface{}); ok {
+			var p paging.Paging
+			var post_id int64
 
-		// 	if cursor, ok := pagingData["cursor"].(string); ok {
-		// 		cursor, err := strconv.Atoi(cursor)
-		// 		if err != nil {
-		// 			log.Printf("Failed to convert cursor to int: %v", err)
-		// 		} else {
-		// 			p.Cursor = &cursor
-		// 		}
-		// 	}
-		// 	p.Validate()
-		// 	posttranport.FindAllByShop(appCtx, &p, d, int(shop_id))
-		// } else {
-		// 	log.Printf("Data in payloadQueue is not of type map[string]interface{}")
-		// }
+			if page, ok := pagingData["page"].(string); ok {
+				page, _ := strconv.Atoi(page)
+				p.Page = page
+			}
+
+			if limit, ok := pagingData["limit"].(string); ok {
+				limit, _ := strconv.Atoi(limit)
+				p.Limit = limit
+			}
+
+			if cursor, ok := pagingData["cursor"].(string); ok {
+				cursor, err := strconv.Atoi(cursor)
+				if err != nil {
+					log.Printf("Failed to convert cursor to int: %v", err)
+				} else {
+					p.Cursor = &cursor
+				}
+			}
+			if post_id_payload, ok := pagingData["post_id"].(float64); ok {
+				post_id = int64(post_id_payload)
+			}
+			p.Validate()
+			messagebrokenrabbitmq.FindAllByPost(appCtx, &p, post_id, d)
+		} else {
+			log.Printf("Data in payloadQueue is not of type map[string]interface{}")
+		}
 	case "increase_one_like":
 		// if id, ok := payloadQueue.Data.(float64); ok {
 		// 	posttranport.IncreaseLike(appCtx, int(id), d)
