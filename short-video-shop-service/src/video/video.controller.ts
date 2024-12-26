244192f93c71aab3b5295d08e1c67a666ce277d9
lehau17 <121428266+lehau17@users.noreply.github.com>
2024-12-26 09:16:01 +0700

    Add controller short video service

--- short-video-shop-service/src/video/video.controller.ts
+++ short-video-shop-service/src/video/video.controller.ts
@@ -3,6 +3,7 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { VideoService } from './video.service';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
+import { PagingDto } from 'src/common/paging/paging';
 
 @Controller()
 export class VideoController {
@@ -14,8 +15,8 @@ export class VideoController {
   }
 
   @MessagePattern('findAllVideo')
-  findAll() {
-    return this.videoService.findAll();
+  findAll(@Payload() paging: PagingDto) {
+    return this.videoService.findAll(paging);
   }
 
   @MessagePattern('findOneVideo')
@@ -25,11 +26,11 @@ export class VideoController {
 
   @MessagePattern('updateVideo')
   update(@Payload() updateVideoDto: UpdateVideoDto) {
-    return this.videoService.update(updateVideoDto.id, updateVideoDto);
+    return this.videoService.update(updateVideoDto);
   }
 
   @MessagePattern('removeVideo')
-  remove(@Payload() id: number) {
-    return this.videoService.remove(id);
+  remove(@Payload() { id, shop_id }: { id: number; shop_id: number }) {
+    return this.videoService.remove(id, shop_id);
   }
 }
