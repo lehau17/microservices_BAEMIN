fe28ca52f82d4bbdb40e5745be7f578bdd3db36b
lehau17 <121428266+lehau17@users.noreply.github.com>
2024-12-26 13:33:31 +0700

    add Cron updateLike

--- short-video-shop-service/src/video/video.service.ts
+++ short-video-shop-service/src/video/video.service.ts
@@ -67,6 +67,53 @@ export class VideoService {
     this.logger.debug('Completed updating views');
   }
 
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async updateLike() {
+    // this.logger.debug('vào cron');
+    const client = this.redisService.getClient();
+    const pattern = 'video_like:*'; // Each video has a key like ' video:{id}'
+    let cursor = 0;
+    const batchSize = 1000;
+
+    const limit = async.queue(async (task: any, done) => {
+      const likeCount = await client.get(task.key);
+      const videoId = task.key.split(':')[1];
+      if (videoId && likeCount) {
+        try {
+          await this.updateLikeInDatabase(+videoId, +likeCount);
+        } catch (error) {
+          this.logger.debug('Lỗi update like');
+        }
+      }
+      await client.del(task.key);
+      done();
+    }, 10); // Limit to 10 concurrent tasks
+
+    do {
+      const [newCursor, keys] = await client.scan(
+        cursor,
+        'MATCH',
+        pattern,
+        'COUNT',
+        batchSize,
+      );
+      if (keys.length <= 0) {
+        this.logger.debug('Không có like để update');
+        return;
+      }
+      cursor = parseInt(newCursor, 10);
+
+      keys.forEach((key) => {
+        limit.push({ key });
+      });
+    } while (cursor !== 0);
+
+    // Wait for all tasks to complete
+    await limit.drain();
+
+    this.logger.debug('Completed updating like');
+  }
+
   private async updateViewInDatabase(video_id: number, view: number) {
     await this.prismaService.videos.update({
       where: {
@@ -80,15 +127,30 @@ export class VideoService {
     });
   }
 
+  private async updateLikeInDatabase(video_id: number, like: number) {
+    await this.prismaService.videos.update({
+      where: {
+        id: video_id,
+      },
+      data: {
+        total_like: {
+          increment: like,
+        },
+      },
+    });
+  }
+
   async increateViewRedis(id: number) {
     const client = this.redisService.getClient();
     const key = `video:${id}`;
     const viewCount = await client.get(key);
+    let result: any;
     if (viewCount && viewCount !== '') {
-      await client.set(key, +viewCount + 1);
+      result = await client.set(key, +viewCount + 1);
     } else {
-      await client.set(key, 1);
+      result = await client.set(key, 1);
     }
+    return result;
   }
 
   async create(createVideoDto: CreateVideoDto): Promise<videos> {
